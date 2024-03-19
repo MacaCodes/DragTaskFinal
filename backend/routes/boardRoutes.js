7438d8c3563dcb4465c094f43413896503c6b8d8
Chiipo <156424376+Chiipo@users.noreply.github.com>
2024-03-19 12:41:22 +0000

    Update boardRoutes.js

--- backend/routes/boardRoutes.js
+++ backend/routes/boardRoutes.js
@@ -35,3 +35,101 @@ router.delete('/tasks/:taskId', authMiddleware.authenticate, async (req, res) =>
 });
 
 module.exports = router;
+
+
+
+//CODE INSPIRED BY THE ONE IN THE VIDEO//
+
+const router = require('express').Router();
+const { body, param } = require('express-validator');
+const validation = require('../handlers/validation');
+const authMiddleware = require('../middleware/authMiddleware');
+const cardController = require('../middleware/cardController');
+
+
+router.post(
+  '/',
+  authMiddleware.verifyToken,
+  [
+    body('title').notEmpty().withMessage('Title is required'),
+    body('description').optional().isString().withMessage('Description must be a string'),
+    body('status').notEmpty().isIn(['To Do', 'In Progress', 'Testing', 'Done']).withMessage('Invalid status'),
+    validation.validate
+  ],
+  cardController.create
+);
+
+router.get(
+  '/',
+  authMiddleware.verifyToken,
+  cardController.getAll
+);
+
+router.put(
+  '/',
+  authMiddleware.verifyToken,
+  cardController.updatePosition
+);
+
+router.get(
+  '/favourites',
+  authMiddleware.verifyToken,
+  cardController.getFavourites
+);
+
+
+router.put(
+  '/favourites',
+  authMiddleware.verifyToken,
+  cardController.updateFavouritePosition
+);
+
+router.get(
+  '/:taskId',
+  [
+    param('taskId').custom(value => {
+      if (!validation.isObjectId(value)) {
+        return Promise.reject('Invalid task ID');
+      } else {
+        return Promise.resolve();
+      }
+    }),
+    validation.validate
+  ],
+  authMiddleware.verifyToken,
+  cardController.getOne
+);
+
+router.put(
+  '/:taskId',
+  [
+    param('taskId').custom(value => {
+      if (!validation.isObjectId(value)) {
+        return Promise.reject('Invalid task ID');
+      } else {
+        return Promise.resolve();
+      }
+    }),
+    validation.validate
+  ],
+  authMiddleware.verifyToken,
+  cardController.update
+);
+
+router.delete(
+  '/:taskId',
+  [
+    param('taskId').custom(value => {
+      if (!validation.isObjectId(value)) {
+        return Promise.reject('Invalid task ID');
+      } else {
+        return Promise.resolve();
+      }
+    }),
+    validation.validate
+  ],
+  authMiddleware.verifyToken,
+  cardController.delete
+);
+
+module.exports = router;
