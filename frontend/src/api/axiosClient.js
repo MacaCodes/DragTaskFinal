4e7ccf075775a7a7a4eacdaea1294fd498494cb9
MacaCodes <gibsonmacauley@gmail.com>
2024-03-20 16:14:51 +0000

    fixedaxiosclientboardapiandapplayout
    axios client, boardapi, applayout. removed auth/token stuff

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -2,21 +2,10 @@ import axios from 'axios'
 import queryString from 'query-string'
 
 const baseUrl = 'http://127.0.0.1:5000/api/v1/'
-const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
   baseURL: baseUrl,
   paramsSerializer: params => queryString.stringify({ params })
-})
-
-axiosClient.interceptors.request.use(async config => {
-  return {
-    ...config,
-    headers: {
-      'Content-Type': 'application/json',
-      'authorization': `Bearer ${getToken()}`
-    }
-  }
 })
 
 axiosClient.interceptors.response.use(response => {
--- frontend/src/components/layout/AppLayout.js
+++ frontend/src/components/layout/AppLayout.js
@@ -1,1 +1,24 @@
 // test 
+
+import { Box } from '@mui/material'
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../common/Sidebar'
+
+const AppLayout = () => {
+  return (
+    <Box sx={{
+      display: 'flex'
+    }}>
+      <Sidebar />
+      <Box sx={{
+        flexGrow: 1,
+        p: 1,
+        width: 'max-content'
+      }}>
+        <Outlet />
+      </Box>
+    </Box>
+  )
+}
+
+export default AppLayout
